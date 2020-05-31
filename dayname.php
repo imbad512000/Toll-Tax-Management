aa37ad9a11d2bb5a7f7c07f266c6c4a188b8ac4e
imbad6273 <17bmiit061@gmail.com>
2020-05-31 22:13:27 +0530

    design change AJAX

--- dayname.php
+++ dayname.php
@@ -1,9 +1,11 @@
-    <select class="form-control ml-0 col-15" name="day_name">
-        <option>Monday</option>
-        <option>Tuesday</option>
-        <option>Wednesday</option>
-        <option>Thursday</option>
-        <option>Friday</option>
-        <option>Saturday</option>
-        <option>Sunday</option>
-    </select>
+    <div class="row">
+        <select class="form-control ml-0 col-15" name="day_name">
+            <option>Monday</option>
+            <option>Tuesday</option>
+            <option>Wednesday</option>
+            <option>Thursday</option>
+            <option>Friday</option>
+            <option>Saturday</option>
+            <option>Sunday</option>
+        </select>
+    </div>
--- monthname.php
+++ monthname.php
@@ -1,14 +1,16 @@
-<select class="form-control ml-0 col-15" name="month_name">
-    <option>January</option>
-    <option>February</option>
-    <option>March</option>
-    <option>April</option>
-    <option>May</option>
-    <option>June</option>
-    <option>July</option>
-    <option>August</option>
-    <option>September</option>
-    <option>October</option>
-    <option>November</option>
-    <option>December</option>
-</select>
+<div class="row">
+    <select class="form-control ml-0 col-15" name="month_name">
+        <option>January</option>
+        <option>February</option>
+        <option>March</option>
+        <option>April</option>
+        <option>May</option>
+        <option>June</option>
+        <option>July</option>
+        <option>August</option>
+        <option>September</option>
+        <option>October</option>
+        <option>November</option>
+        <option>December</option>
+    </select>
+</div>
