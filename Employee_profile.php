94719ba7f69611a7012318e27e5942124441c84b
sidk1105 <57341045+sidk1105@users.noreply.github.com>
2020-06-12 11:42:51 +0530

    data changes

--- Employee_profile.php
+++ Employee_profile.php
@@ -146,7 +146,7 @@
                             <li class="dropdown-divider"></li>
                             <!-- <li class="dropdown-item"><i class="icon-settings mr-2"></i> Setting</li>
                             <li class="dropdown-divider"></li> -->
-                            <li class="dropdown-item"><a href="main.php"><i class="icon-power mr-2"></i> Logout</li></a>
+                            <li class="dropdown-item"><a href="index.php"><i class="icon-power mr-2"></i> Logout</li></a>
                         </ul>
                     </li>
                 </ul>
