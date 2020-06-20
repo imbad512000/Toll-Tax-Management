51c0ddee87957ca0b6704f95f0287e8ff272a7fb
sidk1105 <57341045+sidk1105@users.noreply.github.com>
2020-06-20 13:31:32 +0530

    done

--- emp_reg.php
+++ emp_reg.php
@@ -35,7 +35,7 @@
 				</div>
 				<div class="form-group row">
 					<label class="col-3" for="email">Email</label>
-					<input type="email" name="email" placeholder="Enter Email" class="form-control col-8" required>
+					<input type="email" pattern=(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]) id="em" name="email" placeholder="Enter Email" class="form-control col-8" required>
 				</div>
 				<div class="form-group row">
 					<label class="col-3" for="pass">Password</label>
@@ -51,8 +51,7 @@
 				</div>
 				<div class="form-group row">
 					<label class="col-3" for="dob">Date of Birth</label>
-					<input type="date" name="dob" placeholder="Enter Date of Birth" class="form-control col-8"
-					>
+					<input type="date" name="dob" placeholder="Enter Date of Birth" class="form-control col-8" max="2002-12-31" id="dob">					
 				</div>
 				<div class="form-group row">
 					<label class="col-3" for="dob">Employee Aadhar-Card Number</label>
--- employee_reg.php
+++ employee_reg.php
@@ -238,7 +238,7 @@
                                                         <div class="form-group row">
                                                             <label class="col-3" for="email">Email</label>
                                                             <input type="email" name="email" placeholder="Enter Email"
-                                                                class="form-control col-7" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
+                                                                class="form-control col-7" pattern=(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]) id="em" required>
                                                         </div>
                                                         <div class="form-group row">
                                                             <label class="col-3" for="pass">Password</label>
@@ -260,7 +260,7 @@
                                                         </div>
                                                         <div class="form-group row">
                                                             <label class="col-3" for="dob">Date of Birth</label>
-                                                            <input type="date" name="dob"
+                                                            <input type="date" name="dob" max="2002-12-31"
                                                                 placeholder="Enter Date of Birth"
                                                                 class="form-control col-7">
                                                         </div>
